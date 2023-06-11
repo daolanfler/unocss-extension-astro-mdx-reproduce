621947e90473f563aeca7ff7aac8a9a657b5cb04
daolanfler <daolanfler@gmail.com>
2023-06-11 16:00:28 +0800

    add reproduce case

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx"; // when not using mdx, works fine i.e. 
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Enable Solid to support Solid JSX components.
+  integrations: [mdx()]
+});
